eda955d1477be9a74572370ebda0625d0cb2ed09
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-09 22:34:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,55 @@
-
-
 #include "shell.h"
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         
-        char *line;
+        char *command;
         char **args;
         int status;
 
 
         do {
-            printf("($) ");
-            line = read_line();
-            args = split_line(line);
+            printf("$ ");
+            command = read_command();
+            args = splitLine(command);
 
             if (args[0] != NULL) {
-                status = execute_command(args);
+                status = exe_command(args);
             } else {
                 status = 1;  /* Empty command, continue the loop*/
             }
 
-            free(line);
+            free(command);
             free(args);
         } while (status);
     } else if (argc == 2) {
        
         char *filename = argv[1];
-        char *line;
+        char *command;
         char **args;
 
         size_t bufsize = 0;
 
         FILE *file = fopen(filename, "r");
         if (file == NULL) {
-            perror("Error opening file");
-            return EXIT_FAILURE;
+            perror("Error");
+            return (FAILURE);
         }
 
         
-        while (getline(&line, &bufsize, file) != -1) {
-            args = split_line(line);
-            execute_command(args);
+        while (getline(&command, &bufsize, file) != -1) {
+            args = splitLine(command);
+            exe_command(args);
 
             free(args);
         }
 
         fclose(file);
-        free(line);
+        free(command);
     } else {
         fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
-        return EXIT_FAILURE;
+        return (EXIT_FAILURE);
     }
 
-    return EXIT_SUCCESS;
+    return (SUCCESS);
 }
