66421238fc5c2fd9ec329cea4f39c1157e15cc1d
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-09 20:55:16 +0100

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/* Function to execute a command */
+int execute_command(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        /* Child process */
+        if (execve(args[0], args, environ) == -1) {
+            perror("shell");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        /* Forking error */
+        perror("shell");
+    } else {
+        /* Parent process */
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+/* Function to read a line from the console */
+char *read_line(void) {
+    char *line = NULL;
+    size_t bufsize = 0;
+
+    if (getline(&line, &bufsize, stdin) == -1) {
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS);  /* Ctrl-D pressed, exit the shell */
+        } else {
+            perror("read_line");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+
+/* Function to split a line into tokens */
+char **split_line(char *line) {
+    int bufsize = BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        perror("split_line");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens) {
+                perror("split_line");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
