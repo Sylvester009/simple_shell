4fd23734ac874048a8fce1b86562f174a16367c2
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-09 22:45:44 +0100

    Create execute-command.c

---
+++ execute-command.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+extern char **environ;
+
+int exe_command(char **args) {
+    pid_t pid;
+    int status;
+
+    char *cmd_path = command_path(args[0]);
+
+    if (cmd_path == NULL) {
+        fprintf(stderr, "Shell: Command not found: %s\n", args[0]);
+        return (1);
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        /* Child process */
+        if (execve(cmd_path, args, environ) == -1) {
+            perror("error");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        /* Forking error */
+        perror("error");
+    } else {
+        /* Parent process */
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    free(cmd_path);
+    return (1);
+}
