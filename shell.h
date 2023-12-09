d91628774d8551a92a56382bdca81c974caf9b26
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-09 22:11:12 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -22,6 +22,6 @@ char **splitLine(char *command);
 void exe_command_i(void);
 void exe_command_nil(char *filename);
 
-char *find_command_path(const char *command);
+char *find_command_path(const char *comm);
 
 #endif /* SHELL_H */
