830009ab6d089afa2a7183f5113b23fb8a5b1c77
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-09 22:47:16 +0100

    Create command_path.c

---
+++ command_path.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+char *command_path(const char *comm) {
+    char *path = getenv("PATH");
+    char *token, *temp_path;
+    char *cmd_path = NULL;
+
+    if (path == NULL) {
+        fprintf(stderr, "Shell: PATH environment variable not set\n");
+        return NULL;
+    }
+
+    temp_path = strdup(path);
+    if (temp_path == NULL) {
+        perror("error");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(temp_path, ":");
+    while (token != NULL) {
+        cmd_path = malloc(strlen(token) + strlen(comm) + 2);
+        if (cmd_path == NULL) {
+            perror("error");
+            exit(EXIT_FAILURE);
+        }
+        sprintf(cmd_path, "%s/%s", token, comm);
+
+        if (access(cmd_path, F_OK | X_OK) == 0) {
+            /* Found the executable in the current path*/
+            free(temp_path);
+            return (cmd_path);
+        }
+
+        free(cmd_path);
+        token = strtok(NULL, ":");
+    }
+
+    free(temp_path);
+    fprintf(stderr, "Shell: Command not found in PATH: %s\n", comm);
+    return NULL;
+}
