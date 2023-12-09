d09683fc8d19ecc67fe9353c9397976451b87d7b
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-09 21:29:12 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         /*Interactive mode*/
-        run_interactive_mode();
+        exe_command_i();
     } else if (argc == 2) {
         /* Non-interactive mode*/
-        run_non_interactive_mode(argv[1]);
+        exe_command_nil(argv[1]);
     } else {
         fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
         exit(EXIT_FAILURE);
