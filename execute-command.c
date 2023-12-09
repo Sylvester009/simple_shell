d6f27a21ed9bbfe2e00cf460cf1b69af85be1fca
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-09 22:54:17 +0100

    Update execute-command.c

--- execute-command.c
+++ execute-command.c
@@ -5,8 +5,15 @@ extern char **environ;
 int exe_command(char **args) {
     pid_t pid;
     int status;
+    char *cmd_path;
 
-    char *cmd_path = command_path(args[0]);
+    if (args[0] != NULL && strcmp(args[0], "exit") == 0) {
+        /* Handles the exit built-in command*/
+        printf("Shell: Exiting...\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    cmd_path = command_path(args[0]);
 
     if (cmd_path == NULL) {
         fprintf(stderr, "Shell: Command not found: %s\n", args[0]);
