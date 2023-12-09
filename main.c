889dff249444192de851833484518d3607809430
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-09 22:22:53 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,56 @@
+
+
+#include "shell.h"
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        // Interactive mode
+        char *line;
+        char **args;
+
+      size_t bufsize = 0;
+
+        do {
+            printf("($) ");
+            line = read_line();
+            args = split_line(line);
+
+            if (args[0] != NULL) {
+                status = execute_command(args);
+            } else {
+                status = 1;  // Empty command, continue the loop
+            }
+
+            free(line);
+            free(args);
+        } while (status);
+    } else if (argc == 2) {
+        // Non-interactive mode
+        char *filename = argv[1];
+        char *line;
+        char **args;
+        int status;
+
+        FILE *file = fopen(filename, "r");
+        if (file == NULL) {
+            perror("Error opening file");
+            return EXIT_FAILURE;
+        }
+
+        
+        while (getline(&line, &bufsize, file) != -1) {
+            args = split_line(line);
+            execute_command(args);
+
+            free(args);
+        }
+
+        fclose(file);
+        free(line);
+    } else {
+        fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
