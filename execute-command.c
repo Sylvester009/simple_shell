fe6c5f81f0dac93dd7b72775e25aebbf5a7a360d
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-12 18:10:23 +0100

    Update execute-command.c

--- execute-command.c
+++ execute-command.c
@@ -6,27 +6,19 @@ int exe_command(char **args) {
     pid_t pid;
     int status;
     char *cmd_path;
-     char **env_variable = environ;
 
     if (args[0] != NULL) {
-        /* Handles built-in commands*/
         if (strcmp(args[0], "exit") == 0) {
-            /* Handles the exit built-in command*/
             exit_shell(args);
         } else if (strcmp(args[0], "env") == 0) {
-            /* Handle the env built-in command */
-            env_variable = environ;
-            while (*env_variable != NULL) {
-                printf("%s\n", *env_variable);
-                env_variable++;
+            for (char **env_var = environ; *env_var != NULL; env_var++) {
+                printf("%s\n", *env_var);
             }
             return 1;
-            } else if (strcmp(args[0], "setenv") == 0) {
-            
+        } else if (strcmp(args[0], "setenv") == 0) {
             set_env_var(args);
             return 1;
         } else if (strcmp(args[0], "unsetenv") == 0) {
-            
             unset_env_var(args);
             return 1;
         }
@@ -36,26 +28,23 @@ int exe_command(char **args) {
 
     if (cmd_path == NULL) {
         fprintf(stderr, "Shell: Command not found: %s\n", args[0]);
-        return (1);
+        return 1;
     }
 
     pid = fork();
     if (pid == 0) {
-        /* Child process */
         if (execve(cmd_path, args, environ) == -1) {
             perror("error");
+            exit(EXIT_FAILURE);
         }
-        exit(EXIT_FAILURE);
     } else if (pid < 0) {
-        /* Forking error */
         perror("error");
     } else {
-        /* Parent process */
         do {
             waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 
     free(cmd_path);
-    return (1);
+    return 1;
 }
