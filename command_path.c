e16b340bb5928719b033ad946a1e65cbbcf8bc4b
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-13 11:32:52 +0100

    Update command_path.c

--- command_path.c
+++ command_path.c
@@ -6,36 +6,39 @@ char *command_path(const char *comm) {
     char *cmd_path = NULL;
 
     if (path == NULL) {
-        fprintf(stderr, "./shell: No such file or directory\n");
+        fprintf(stderr, "Shell: No PATH variable found\n");
         return NULL;
     }
 
     temp_path = strdup(path);
     if (temp_path == NULL) {
-        perror("./shell: No such file or directory");
+        perror("Shell");
         exit(EXIT_FAILURE);
     }
 
     token = _strtok(temp_path, ":");
+    
+    /*Allocate cmd_path outside the loop*/
+    cmd_path = malloc(strlen(comm) + 1);
+    if (cmd_path == NULL) {
+        perror("Shell");
+        exit(EXIT_FAILURE);
+    }
+
     while (token != NULL) {
-        cmd_path = malloc(strlen(token) + strlen(comm) + 2);
-        if (cmd_path == NULL) {
-            perror("./shell: No such file or directory");
-            exit(EXIT_FAILURE);
-        }
         sprintf(cmd_path, "%s/%s", token, comm);
 
         if (access(cmd_path, F_OK | X_OK) == 0) {
             /* Found the executable in the current path*/
             free(temp_path);
-            return (cmd_path);
+            return cmd_path;
         }
 
-        free(cmd_path);
         token = _strtok(NULL, ":");
     }
 
+    free(cmd_path);  /* Free only if the executable is not found*/
     free(temp_path);
-    fprintf(stderr, "./shell: No such file or directory %s\n", comm);
+    fprintf(stderr, "Shell: No such file or directory: %s\n", comm);
     return NULL;
 }
