8176d8c32dc1340d061e025f64fa039b7b5127a7
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-13 11:22:17 +0100

    Update execute-command.c

--- execute-command.c
+++ execute-command.c
@@ -6,13 +6,12 @@ int exe_command(char **args) {
     pid_t pid;
     int status;
     char *cmd_path;
-    char **env_var;
 
     if (args[0] != NULL) {
         if (strcmp(args[0], "exit") == 0) {
             exit_shell(args);
         } else if (strcmp(args[0], "env") == 0) {
-            for (env_var = environ; *env_var != NULL; env_var++) {
+            for (char **env_var = environ; *env_var != NULL; env_var++) {
                 printf("%s\n", *env_var);
             }
             return 1;
@@ -28,18 +27,18 @@ int exe_command(char **args) {
     cmd_path = command_path(args[0]);
 
     if (cmd_path == NULL) {
-        fprintf(stderr, "./shell: No such file or directory %s\n", args[0]);
+        fprintf(stderr, "Shell: Command not found: %s\n", args[0]);
         return 1;
     }
 
     pid = fork();
     if (pid == 0) {
         if (execve(cmd_path, args, environ) == -1) {
-            perror("./shell: No such file or directory");
+            perror("execve");
             exit(EXIT_FAILURE);
         }
     } else if (pid < 0) {
-        perror("./shell: No such file or directory");
+        perror("fork");
     } else {
         do {
             waitpid(pid, &status, WUNTRACED);
