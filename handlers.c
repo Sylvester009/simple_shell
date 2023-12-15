989cc18318e4acd3eca170baefad41aa14a02e75
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-15 09:06:23 +0100

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -19,3 +19,15 @@ size_t _strlen(const char *string)
 
     return len;
 }
+
+/**
+ * ctrl_c_handler - handles the signal raised by CTRL-C
+ * @signum: signal number
+ *
+ * Return: void
+ */
+void ctrl_c_handler(int signum)
+{
+	if (signum == SIGINT)
+		print("\n($) ", STDIN_FILENO);
+}
