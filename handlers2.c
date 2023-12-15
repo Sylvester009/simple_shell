aaffbbef714c60cd6e8f7d85ce7f4de5c1926e4c
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-15 11:44:59 +0100

    Update handlers2.c

--- handlers2.c
+++ handlers2.c
@@ -45,7 +45,7 @@ int _strcspn(char *str, char *reject) {
  *
  * Return: pointer to the first occurrence of `c` in `s`
  */
-char *custom_strchr(char *s, char c) {
+char *_strchr(char *s, char c) {
     int i = 0;
 
     for (; s[i] != c && s[i] != '\0'; i++)
@@ -55,3 +55,60 @@ char *custom_strchr(char *s, char c) {
     else
         return NULL;
 }
+
+/**
+ * _strcpy - Copies a string to another buffer
+ * @src: Source to copy from
+ * @dest: Destination to copy to
+ *
+ * Return: void
+ */
+void _strcpy(char *src, char *dest)
+{
+    int i = 0;
+
+    for (; src[i] != '\0'; i++)
+        dest[i] = src[i];
+    dest[i] = '\0';
+}
+
+/**
+ * _strcmp - Compare two strings
+ * @str1: First string to be compared
+ * @str2: Second string to be compared
+ *
+ * Return: Difference of the two strings
+ */
+int _strcmp(char *str1, char *str2)
+{
+    int i = 0;
+
+    while (str1[i] != '\0')
+    {
+        if (str1[i] != str2[i])
+            break;
+        i++;
+    }
+    return (str1[i] - str2[i]);
+}
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: String to be concatenated to
+ * @src:  String to concatenate
+ *
+ * Return: Address of the new string
+ */
+char *_strcat(char *dest, char *src)
+{
+    char *result = NULL;
+    int len_dest = _strlen(dest);
+    int len_src = _strlen(src);
+
+    result = malloc(sizeof(*result) * (len_dest + len_src + 1));
+    _strcpy(dest, result);
+    _strcpy(src, result + len_dest);
+    result[len_dest + len_src] = '\0';
+    return result;
+}
+
